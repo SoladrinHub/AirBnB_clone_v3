1923ed0ea82dba40779601517bb822831bd34ec4
SoladrinHub <cointoken8021@gmail.com>
2024-08-09 00:16:09 -0700

    updated file

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,25 +1,46 @@
-
 #!/usr/bin/python3
 """
-Starts a Flask web application
+index
 """
+
 from flask import jsonify
-'''from models import storage'''
 from api.v1.views import app_views
 
+from models import storage
 
-@app_views.route('/status', strict_slashes=False)
+
+@app_views.route("/status", methods=['GET'], strict_slashes=False)
 def status():
-    """display the status response"""
-    return jsonify(status="OK")
+    """
+    status route
+    :return: response with json
+    """
+    data = {
+        "status": "OK"
+    }
+
+    resp = jsonify(data)
+    resp.status_code = 200
+
+    return resp
 
 
-@app_views.route('/stats', strict_slashes=False)
-"""This code has the advantage of not returning non-existing objects"""
+@app_views.route("/stats", methods=['GET'], strict_slashes=False)
 def stats():
-    """display the number of each objects by type"""
-    all_classes = {"Amenity": "amenities", "City": "cities", "Place": "places",
-                   "Review": "reviews", "State": "states", "User": "users"}
-    return jsonify({v: storage.count(k) for k, v in all_classes.items()
-                    if storage.count(k)})
+    """
+    stats of all objs route
+    :return: json of all objs
+    """
+    data = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User"),
+    }
 
+    resp = jsonify(data)
+    resp.status_code = 200
+
+    return resp
